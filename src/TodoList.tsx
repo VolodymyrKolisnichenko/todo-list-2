2f92e60d807cd0d626dbfe3f4aa08d1ab3893763
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-16 00:10:45 +0200

    Update TodoList.tsx
    added onKeyDown in value

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -22,9 +22,13 @@ export function TodoList(props: PropsType) {
     return <div>
       <h3>{props.title}</h3>
       <div>
-        <input value={newTaskTitle} onChange={(e) => {
-          setNewTaskTitle(e.currentTarget.value)
-          }}/>
+        <input 
+        value={newTaskTitle} 
+        onChange={(e) => {setNewTaskTitle(e.currentTarget.value)}}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+        props.addTask(newTaskTitle);
+        setNewTaskTitle("")}}}/>
         <button onClick={() => {
           props.addTask(newTaskTitle) 
           setNewTaskTitle("")}}>+</button>
