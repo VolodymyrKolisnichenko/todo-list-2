ae55a4ffd7ccc8bc40271e5f3f5ffa8b600e22b9
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-25 21:55:40 +0200

    Update TodoList.tsx
    cleared

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -50,33 +50,6 @@ export function TodoList(props: PropsType) {
     }
   };
 
- function solution(number: number): string {
-  const romanSymbols: {value: number, symbol: string}[] = [
-    { value: 1000, symbol: 'M' },
-    { value: 900, symbol: 'CM' },
-    { value: 500, symbol: 'D' },
-    { value: 400, symbol: 'CD' },
-    { value: 100, symbol: 'C' },
-    { value: 90, symbol: 'XC' },
-    { value: 50, symbol: 'L' },
-    { value: 40, symbol: 'XL' },
-    { value: 10, symbol: 'X' },
-    { value: 9, symbol: 'IX' },
-    { value: 5, symbol: 'V' },
-    { value: 4, symbol: 'IV' },
-    { value: 1, symbol: 'I' },
-  ]
- let result: string = '';
- let count= number;
- romanSymbols.map((current, index) => {
-  while (count >= current.value) {
-    result += current.symbol;
-    count -= current.value
-  }
- })
- return result
-}
-console.log(solution(1444))
 
   const onAllClickHandler = () => props.changeFilter("all", props.id);
   const onActiveClickHandler = () => props.changeFilter("active", props.id);
